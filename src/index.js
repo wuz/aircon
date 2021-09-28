6e38fe7e52c2c849c60ad526e2da25df27965b73
wuz <conlin@figurehr.com>
2021-09-28 10:14:12 -0400

    add video streaming route

--- src/index.js
+++ src/index.js
@@ -1,33 +1,66 @@
+const { create } = require("ipfs");
 const fastify = require("fastify")({ logger: true });
 const { fastifyFunky } = require("fastify-funky");
-const formidable = require("formidable");
+const Stream = require("stream");
 const path = require("path");
+const { Buffer } = require("buffer");
+const FileType = require("file-type");
 
+const { extract } = require("it-tar");
+const { pipe } = require("it-pipe");
+const toBuffer = require("it-to-buffer");
+const all = require("it-all");
+const map = require("it-map");
+
+let ipfs;
+
+fastify.register(require("fastify-multipart"));
 fastify.register(fastifyFunky);
 fastify.register(require("fastify-static"), {
   root: path.join(__dirname, "public"),
-  prefix: "/public/" // optional: default '/'
+  prefix: "/public/", // optional: default '/'
 });
+
+async function* tarballed(source) {
+  yield* pipe(source, extract(), async function* (source) {
+    for await (const entry of source) {
+      yield {
+        ...entry,
+        body: await toBuffer(map(entry.body, (buf) => buf.slice())),
+      };
+    }
+  });
+}
+
+async function collect(source) {
+  return all(source);
+}
+
+const ipfsToFile = async (cid) => await pipe(ipfs.get(cid), tarballed, collect);
 
 // Declare a route
 fastify.get("/", async (req, res) => {
   return { hello: "world" };
 });
 
-fastify.post("/api/upload", async (req, res) => {
-  const form = formidable({ multiples: true });
+fastify.get("/api/view/:cid", async (req, reply) => {
+  const { cid } = req.params;
 
-  form.parse(req, (err, fields, files) => {
-    res.writeHead(200, { "content-type": "application/json" });
-    res.end(JSON.stringify({ fields, files }, null, 2));
-  });
+  const fileData = await ipfsToFile(cid);
+  const buffer = Buffer.from(fileData[0].body.buffer);
+  const fileType = await FileType.fromBuffer(buffer);
+  reply.type(fileType).send(buffer);
+});
 
-  return;
+fastify.post("/api/upload", async (req, reply) => {
+  const data = await req.file();
+  return data.filename;
 });
 
 const start = async () => {
+  ipfs = await create();
   try {
-    await fastify.listen(8080);
+    await fastify.listen(4040);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
